e0566dec62ae7852f5681d79430f1166f3a9579a
kgv7 <kaitlyn.a.goodman@gmail.com>
2022-02-26 14:54:45 -0800

    added conditions to mortgage calc

--- src/MortgageCalc.java
+++ src/MortgageCalc.java
@@ -5,15 +5,42 @@ class MortgageCalculator {
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
 
-        System.out.print("Annual Interest Rate: ");
-        float rate = scanner.nextFloat();
+        int principal = 0;
+        float rate = 0;
+        byte years = 0;
+        
+        while (true){
+            System.out.print("Principal ($1k - $1m): ");
+            principal = scanner.nextInt();
+        if (1000 >= principal || principal >= 1_000_000){
+            System.out.println("You must input a principal between 1,000 and 1,000,000.");
+        } else {
+            break;
+        }
+        }
 
-        System.out.print("Period (Years): ");
-        byte years = scanner.nextByte();
+        while (true){
+            System.out.print("Annual Interest Rate: ");
+            rate = scanner.nextFloat();
+            if (0 >= rate || rate > 30){
+                System.out.println("Enter a value greater than 0 and less than or equal to 30.");
+            } else {
+                break;
+            }
+        }
+        
+        while (true){
+            System.out.print("Period (Years): ");
+            years = scanner.nextByte();
 
+            if (1 > years || 30 < years){
+                System.out.println("Enter a value between 1 and 30.");
+            } else {
+                break;
+            }
+        }
+        
         float monthlyRate = (rate/100)/12;
 
         double mortgage = principal * ( (monthlyRate * (Math.pow((1 + monthlyRate), (years * 12)))/ ((Math.pow((1 + monthlyRate), (years * 12)))-1 ) ));
